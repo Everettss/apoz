be651998722b94af25b4584949b84e8154353f1b
deliriusz <kalinowskirafal.fm@gmail.com>
2018-01-26 22:29:09 +0100

    adding possibility to retrieve start and end click position

--- src/Image/Image.js
+++ src/Image/Image.js
@@ -1,8 +1,43 @@
 import React, { Component } from 'react';
 import './Image.css'
+import Modal from 'react-modal';
 import savePixels from 'save-pixels';
 
+const getMousePos = (canvas, evt) => {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left + 20,
+        y: evt.clientY - rect.top + 20
+    };
+};
+
 class Image extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            lineStartPoint: {x: 0, y: 0},
+            lineEndPoint: {x: 0, y: 0},
+        };
+
+        this.setLineProfileStartPoint = this.setLineProfileStartPoint.bind(this);
+        this.setLineProfileEndPoint = this.setLineProfileEndPoint.bind(this);
+    }
+
+    setLineProfileStartPoint (ev) {
+        let canvas = this.canvasWrapper.childNodes.item(0);
+        let clickPoint = getMousePos(canvas, ev);
+        this.setState({lineStartPoint: clickPoint});
+        console.log ("ev click on start" + clickPoint.x);
+    }
+
+    setLineProfileEndPoint (ev) {
+        let canvas = this.canvasWrapper.childNodes.item(0);
+        let clickPoint = getMousePos(canvas, ev);
+        console.log ("ev click on end" + clickPoint.x);
+        this.setState({lineEndPoint: clickPoint});
+    }
+
     componentWillReceiveProps(nextProps) {
         if (
             !this.props.data ||
@@ -22,7 +57,10 @@ class Image extends Component {
 
     render() {
         return (
-            <div className="canvas-wrapper" ref={(canvasWrapper) => { this.canvasWrapper = canvasWrapper; }} />
+            <div className="canvas-wrapper"
+                 onMouseDown={this.setLineProfileStartPoint.bind(this)}
+                 onMouseUp={this.setLineProfileEndPoint.bind(this)}
+                 ref={(canvasWrapper) => { this.canvasWrapper = canvasWrapper; }} />
         );
     }
 }
