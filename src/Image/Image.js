fe9e10a35f92d016225e4b0e6d568bc5ee7e33d8
deliriusz <kalinowskirafal.fm@gmail.com>
2018-01-26 23:16:15 +0100

    working on LineProfile modal

--- src/Image/Image.js
+++ src/Image/Image.js
@@ -4,11 +4,22 @@ import Modal from 'react-modal';
 import savePixels from 'save-pixels';
 
 const getMousePos = (canvas, evt) => {
-    var rect = canvas.getBoundingClientRect();
+    let rect = canvas.getBoundingClientRect();
     return {
         x: evt.clientX - rect.left + 20,
         y: evt.clientY - rect.top + 20
     };
+};
+
+const customStyles = {
+    content : {
+        top                   : '50%',
+        left                  : '50%',
+        right                 : 'auto',
+        bottom                : 'auto',
+        marginRight           : '-50%',
+        transform             : 'translate(-50%, -50%)'
+    }
 };
 
 class Image extends Component {
@@ -18,25 +29,57 @@ class Image extends Component {
         this.state = {
             lineStartPoint: {x: 0, y: 0},
             lineEndPoint: {x: 0, y: 0},
+            modalIsOpen: false,
+            subimage: null
         };
 
         this.setLineProfileStartPoint = this.setLineProfileStartPoint.bind(this);
         this.setLineProfileEndPoint = this.setLineProfileEndPoint.bind(this);
+
+        this.openModal = this.openModal.bind(this);
+        this.afterOpenModal = this.afterOpenModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
     }
 
     setLineProfileStartPoint (ev) {
         let canvas = this.canvasWrapper.childNodes.item(0);
         let clickPoint = getMousePos(canvas, ev);
         this.setState({lineStartPoint: clickPoint});
-        console.log ("ev click on start" + clickPoint.x);
     }
 
     setLineProfileEndPoint (ev) {
         let canvas = this.canvasWrapper.childNodes.item(0);
         let clickPoint = getMousePos(canvas, ev);
-        console.log ("ev click on end" + clickPoint.x);
-        this.setState({lineEndPoint: clickPoint});
+        let context = canvas.getContext('2d');
+        let subImageData = context.getImageData(
+            this.state.lineStartPoint.x,
+            this.state.lineStartPoint.y,
+            clickPoint.x,
+            clickPoint.y
+        );
+
+        this.setState({
+            lineEndPoint: clickPoint,
+            modalIsOpen: true,
+            subimage: subImageData,
+        });
+        console.log(subImageData);
     }
+
+
+    openModal() {
+        this.setState({modalIsOpen: true});
+    }
+
+    afterOpenModal() {
+        // references are now sync'd and can be accessed.
+        this.subtitle.style.color = '#f00';
+    }
+
+    closeModal() {
+        this.setState({modalIsOpen: false});
+    }
+
 
     componentWillReceiveProps(nextProps) {
         if (
@@ -60,7 +103,19 @@ class Image extends Component {
             <div className="canvas-wrapper"
                  onMouseDown={this.setLineProfileStartPoint.bind(this)}
                  onMouseUp={this.setLineProfileEndPoint.bind(this)}
-                 ref={(canvasWrapper) => { this.canvasWrapper = canvasWrapper; }} />
+                 ref={(canvasWrapper) => { this.canvasWrapper = canvasWrapper; }} >
+                <div>
+                    <Modal
+                        isOpen={this.state.modalIsOpen}
+                        onAfterOpen={this.afterOpenModal}
+                        onRequestClose={this.closeModal}
+                        style={customStyles}
+                        contentLabel="Example Modal">
+                        <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
+                        <button onClick={this.closeModal}>close</button>
+                        <div></div>
+                    </Modal>
+                </div></div>
         );
     }
 }
